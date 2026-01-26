e1b9871d3465dea210131142d3b064119890a60d
hojeong <parkhj062@gmail.com>
2026-01-26 15:17:12 +0900

    initial commit

---
+++ src/main/java/hello/studyall/StudyallApplication.java
@@ -0,0 +1,13 @@
+package hello.studyall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/studyall/StudyallApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.studyall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
