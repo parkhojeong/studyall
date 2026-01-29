a06c267eb9e16a80d326fe5f65970729f0dc66e7
hojeong <parkhj062@gmail.com>
2026-01-29 22:33:50 +0900

    [김영한의 실전 데이터베이스 - 기본편 - 33. UNION]

--- src/main/resources/db_basic_schema.sql
+++ src/main/resources/db_basic_schema.sql
@@ -82,3 +82,14 @@ INSERT INTO sizes(size) VALUES
 ('S'), ('M'), ('L'), ('XL');
 INSERT INTO colors(color) VALUES
 ('Red'), ('Blue'), ('Black');
+
+DROP TABLE IF EXISTS retired_users;
+CREATE TABLE retired_users (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    retired_date DATE NOT NULL
+);
+INSERT INTO retired_users (id, name, email, retired_date) VALUES
+    (1, '션', 'sean@example.com', '2024-12-31'),
+    (7, '아이작 뉴턴', 'newton@example.com', '2025-01-10');
