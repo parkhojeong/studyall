4b0a3fdd02f438d44ccfae1a501bd3571b0ac601
parkhojeong <parkhj062@gmail.com>
2026-02-11 01:18:30 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편 - 8. Hello JPA - 애플리케이션 개발]

--- ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -5,5 +5,21 @@ import jakarta.persistence.*;
 public class JpaMain {
 
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        try {
+            // query
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
